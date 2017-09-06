1a71e05fc233bdd7aa730b981fbabc4947f04948
Jun Fukumoto <j.fumofumo@gmail.com>
2017-09-07 00:44:42 +0900

    練習問題回答

---
+++ Practice1.java
@@ -0,0 +1,22 @@
+/**
+ *
+ */
+
+
+/**
+ * @author Fumomo
+ * 練習問題 1 - 1
+ *  Hello World”と表示するプログラムを作成しなさい。
+ */
+public class Practice1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("Hello, World!");
+
+	}
+
+}
---
+++ Practice2.java
@@ -0,0 +1,27 @@
+/**
+ *
+ */
+
+/**
+ * @author Fumomo
+ * 練習問題 1 - 2
+ * int 型の変数 x に数値 11 を代入し、
+ * x の値を“x=11”のように表示するプログラムを作成しなさい。
+ */
+public class Practice2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int x;
+		
+		x = 11;
+		
+		System.out.println(x);
+	
+
+	}
+
+}
